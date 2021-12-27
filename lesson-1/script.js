3034031c59ce9aef70e1099bf5f40f1ff9a9f227
Alexey Gulo <alexeygulo@gmail.com>
2021-12-27 23:48:02 +0300

    Lesson 1

---
+++ lesson-1/script.js
@@ -0,0 +1,134 @@
+// console.log('Hello')
+
+// Переменные
+// let word = 'Hello'
+// word = 'Test'
+
+// console.log(word)
+
+// const pi = 3.14
+// pi = 4 error
+// console.log(pi)
+
+// Типы данных
+// String '', ' ', '12', 'string'
+// Number 0, Infinity, -Infinity, 12, 5, 5.5, NaN
+// Boolean true/false
+// Null null
+// Undefined undefined
+// Object {} [] function regexp
+
+// Математические операторы
+// +, -, *, /, **, %, Math
+// console.log(24 / 3 + 10)
+
+// let counter = 0
+// counter = counter + 1
+// counter += 1
+// Инкремент
+// counter++ // постфиксная
+// ++counter // префиксная
+
+// let sum = 0
+// let resultSum = sum++
+// let resultSum = ++sum
+// console.log(resultSum)
+// console.log(counter)
+
+// Декремент
+// counter-- // постфиксная
+// --counter // префиксная
+
+// Конкатенация строк
+// const startString = 'Hello user'
+// console.log(startString + ' ' + 'Alex')
+// console.log(startString + ' ' + 'Vadim')
+
+// Операторы сравнения
+// >, <, <=, >=, ==, ===, !=, !==
+// console.log(10 < 20) // true
+// console.log(0 > -5) // true
+// console.log(25 <= 23) // false
+// console.log(25 >= 23) // true
+
+// Неявное преобразование типов (String, Number, Boolean)
+
+// console.log(10 < '20') // true
+// console.log(null == undefined) // true
+// console.log('' > 10) // false
+// console.log(null - 5) // -5
+// console.log('string ' + 100) // 'string 100'
+
+// console.log('aбвг' < 'aбвд') // true
+
+
+// Условия
+// let b = 10
+// if (b < 20) {
+//   console.warn('b меньше 20')
+// } else {
+//   console.log('b больше 20')
+// }
+
+// -------0++++++++++20--------
+
+// if (b < 20 || b > 0 && b != null) { // true / false
+//   console.warn('b меньше 20')
+// } else if (b > 20) {
+//   console.log('b больше 20')
+// } else if (b == 20) {
+//   console.log('b равно 20')
+// }
+
+// Найти наибольшее число из:
+// let a = 15;
+// let b = -8;
+// let c = 23;
+
+// if (a > b && a > c) {
+//   console.log(a)
+// } else if (b > a && b > c) {
+//   console.log(b)
+// } else if (c > a && c > b) {
+//   console.log(c)
+// }
+
+// Циклы
+// const number = 5
+// 1 + 2 + 3 + 4 + 5 = 15
+
+// let i = 1
+// let result = 0
+// while (i <= number) {
+//   result = result + i
+//   i++
+// }
+
+// console.log(result)
+
+// do {
+//   result = result + i
+//   i++
+// } while (i <= number)
+
+// console.log(result)
+
+// for (let i = 1; i <= number; i++) {
+//   result = result + i
+//   console.log(result)
+// }
+
+// console.log(result)
+
+// const string1 = 'Apple'
+
+// for (let i = 0; i < string1.length; i++) {
+//   console.log(string1[i])
+// }
+
+// const isConfirm = confirm('Вам есть 18 лет') // true / false
+// console.log(isConfirm)
+
+
+// const promptResult = +prompt('Введите ваш возраст', 0)
+// console.log(promptResult)
